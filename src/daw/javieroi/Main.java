37df36ec99a79b547988c44bf8c4d08a892cab02
javier <ocagnen2@gmail.com>
2019-04-24 17:56:47 +0200

    inicio

---
+++ src/daw/javieroi/Main.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package daw.javieroi;
+
+/**
+ *
+ * @author Javier
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        Main.multiplicar(2, 10);
+        
+        System.out.println("--------------------------------");
+
+        int[] array = {1, 2, 3, 3, 2, 1};
+        int[] array2 = {1, 3, 2, 6, 9, 7};        
+        boolean prueba = Main.simetrico(array, 0);
+        System.out.println(prueba);
+        
+        System.out.println("---------------------------------------");
+        Main.burbuja(array2, 0,0);
+
+        for (int i : array2) {
+            System.out.println(i);
+        }
+
+    }
+
+    public static int multiplicar(int numero, int veces) {
+
+        if (veces == 0) {
+            return numero * veces;
+        }
+
+        System.out.println(numero * veces);
+
+        return multiplicar(numero, veces - 1);
+
+    }
+
+    public static boolean simetrico(int[] a, int posicion) {
+
+        if ((a[(a.length) - 1 - posicion] == a[posicion]) && a.length / 2 >= posicion) {
+
+            return simetrico(a, posicion + 1);
+        }
+
+        if (a.length / 2 <= posicion) {
+            return true;
+        }
+
+        return false;
+
+    }
+
+    public static boolean burbuja(int[] a, int posicion, int numeroevaluar) {
+
+        int tmp;
+
+        if (a[numeroevaluar] > a[posicion]) {
+            tmp = a[numeroevaluar];
+            a[numeroevaluar] = a[posicion];
+            a[posicion] = tmp;
+        } else if (posicion < a.length-1) {
+            return burbuja(a, posicion + 1, numeroevaluar);
+        }
+        
+        
+
+        if (numeroevaluar < a.length - 1) {
+            return burbuja(a, numeroevaluar+1, numeroevaluar + 1);
+        }
+        
+        return false;
+    }
+}
